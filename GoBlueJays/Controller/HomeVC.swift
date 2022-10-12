82f284d32aa9abacba1837fe1dc9eef4b43d65b4
heedxliu168 <xliu168@jh.edu>
2022-10-12 16:43:19 -0400

    revert succ

--- GoBlueJays/Controller/HomeVC.swift
+++ GoBlueJays/Controller/HomeVC.swift
@@ -15,11 +15,11 @@ class HomeVC: UIViewController {
         navigationController?.additionalSafeAreaInsets.top = -5
     }
     @IBAction func tapBrody(_ sender: UIButton) {
-        UIApplication.shared.openURL(URL(string: "https://jhu.libcal.com/spaces?lid=1195&gid=2086&c=0")!)
+        UIApplication.shared.open(URL(string: "https://jhu.libcal.com/spaces?lid=1195&gid=2086&c=0")!)
     }
     
     @IBAction func tapMSE(_ sender: UIButton) {
-        UIApplication.shared.openURL(URL(string: "https://jhu.libcal.com/seats?lid=1196")!)
+        UIApplication.shared.open(URL(string: "https://jhu.libcal.com/seats?lid=1196")!)
     }
     @IBAction func Transloc(_ sender: Any) {
         UIApplication.shared.open(URL(string: "https://apps.apple.com/us/app/transloc/id1280444930")! as URL, options: [:],completionHandler: nil)
@@ -29,9 +29,9 @@ class HomeVC: UIViewController {
         UIApplication.shared.open(URL(string: "https://apps.apple.com/us/app/transact-mobile-ordering/id1494719529")! as URL, options: [:],completionHandler: nil)
     }
     
-    @IBAction func CampusGroups(_ sender: Any) {
+    @IBAction func Canvas(_ sender: Any) {
         UIApplication.shared.open(URL(string: "https://canvas.jhu.edu")! as URL, options: [:],completionHandler: nil)
-    }
+    } //https://apps.apple.com/us/app/canvas-student/id480883488 use this link instead
     
     @IBAction func SIS(_ sender: Any) {
         UIApplication.shared.open(URL(string: "https://sis.jhu.edu/sswf/")! as URL, options: [:],completionHandler: nil)
