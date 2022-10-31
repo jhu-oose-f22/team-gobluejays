b2233fccfba56159fbdc9142442ba13c239eca15
Jessie Luo <jluo30@jhu.edu>
2022-10-31 00:40:44 -0400

    displaying on schedule

--- GoBlueJays/Controller/ScheduleVC.swift
+++ GoBlueJays/Controller/ScheduleVC.swift
@@ -9,14 +9,13 @@ import UIKit
 import FirebaseCore
 import FirebaseFirestore
 
-class ScheduleVC: UIViewController,NewEventDelegate {
-    var width = 57.5
-    var hour = 53.0
-    var eight = 162.0
-    var monday = 96.0
+class ScheduleVC: UIViewController{
     var text: String?
+    var currentTerm: String?
     var week = ["1", "2", "3", "2", "3"]
     var courses: [Course] = []
+    var registeredCourses: [RegisteredCourse] = []
+    var currentWeekCourses:[Book] = []
     
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
@@ -30,19 +29,12 @@ class ScheduleVC: UIViewController,NewEventDelegate {
     @IBOutlet weak var THU: UILabel!
     @IBOutlet weak var FRI: UILabel!
     @IBOutlet weak var term: UIButton!
-    func newEventDetail(name:String, start: Double, end: Double, date: Double) {
-        print(start)
-        print(end)
-        print(date)
-        view.addSubview(event(title:name, day: date, start: start, duration: (end-start)))
-//        view.addSubview(welcomeLabel(title:name, day: date, start: start, duration: 2))
-    }
     
     func setCurrentWeek(date: Date) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEE, MMMM dd, yyyy" // OR "dd-MM-yyyy"
         let dateFormatter2 = DateFormatter()
-        dateFormatter2.dateFormat = "dd/MM/yyyy"
+        dateFormatter2.dateFormat = "MM/dd/yyyy"
         
         let month = dateFormatter2.string(from: date).substring(with: 3..<5)
         var monthString = "";
@@ -74,7 +66,6 @@ class ScheduleVC: UIViewController,NewEventDelegate {
           default:
             monthString = "default"
         }
-        //term.setTitle(monthString, for: .normal)
         
         var mondayIndex = 0;
         let currentDateString: String = dateFormatter.string(from: date)
@@ -102,13 +93,55 @@ class ScheduleVC: UIViewController,NewEventDelegate {
             week[i] = dateFormatter2.string(from: dateNext)
         }
 
-        MON.text = week[0].substring(with: 0..<2)
-        TUE.text = week[1].substring(with: 0..<2)
-        WED.text = week[2].substring(with: 0..<2)
-        THU.text = week[3].substring(with: 0..<2)
-        FRI.text = week[4].substring(with: 0..<2)
+        MON.text = week[0].substring(with: 3..<5)
+        TUE.text = week[1].substring(with: 3..<5)
+        WED.text = week[2].substring(with: 3..<5)
+        THU.text = week[3].substring(with: 3..<5)
+        FRI.text = week[4].substring(with: 3..<5)
         print(week[0])
+        currentTerm = "";
+        let currentDate = week[0].substring(with: 0..<5);
+        if (currentDate >= "08/29" && currentDate < "12/09") {
+            
+        }
+//        if (week[0].substring(with: 0..<2) > ""
     }
+    struct Meeting: Decodable {
+        let DOW: String
+        let Dates: String
+        let Times: String
+        let Location: String
+        let Building: String
+        let Room: String
+    }
+    
+    struct SectionDetail: Decodable {
+        let Description: String
+        let Meetings: [Meeting]
+    }
+    
+    struct Book: Decodable {
+        let TermStartDate: String
+        let SchoolName: String
+        //"MW 12:00PM - 1:15PM, Th 3:00PM - 3:50PM",
+        let Meetings: String
+        let OfferingName: String
+        let SectionName: String
+        let Title: String
+        let Credits: String
+        let Level: String
+        let Areas: String
+        let Building: String
+        let Term_JSS: String
+        let SectionDetails : [SectionDetail]
+    }
+    
+    struct RegisteredCourse {
+        let semester: String
+        let courseNumber: String
+    }
+
+    
 
     @IBOutlet weak var dateTF: TextField!
     override func viewDidLoad() {
@@ -128,39 +161,54 @@ class ScheduleVC: UIViewController,NewEventDelegate {
         self.view.addGestureRecognizer(tapGesture)
         //End DatePicker Code
         
-//        let a;
+
         //test
         let db = Firestore.firestore()
+        // fake data
+        // Course1: EN.601.421(01), Fall 2022
+        let registeredCourse1: RegisteredCourse = RegisteredCourse(semester: "Fall%202022", courseNumber: "EN55343601");
+        let registeredCourse2: RegisteredCourse = RegisteredCourse(semester: "Fall%202022", courseNumber: "EN60142101");
+        registeredCourses.append(registeredCourse1);
+        registeredCourses.append(registeredCourse2);
+        for registeredCourse in registeredCourses {
+            let group = DispatchGroup()
+            group.enter()
+            getRandomFood(semester: registeredCourse.semester, courseNumber: registeredCourse.courseNumber){ json, error in
+                self.currentWeekCourses = json ?? []
+                group.leave()
+            }
+            group.wait()
+            print("target1")
+            print(self.currentWeekCourses)
+            displayCourses(books: self.currentWeekCourses)
+        }
         setCurrentWeek(date: Date())
+    }
+    
+    func getRandomFood(semester:String,courseNumber:String, completion: @escaping (_ json: [Book]?, _ error: Error?)->()) {
+        var booooks:[Book] = []
         
-        db.collection("calendarCourse").getDocuments(){ [self]
-            (QuerySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in QuerySnapshot!.documents {
-                    let data = document.data()
-                    guard let dateee = data["date"] as? String else {
-                        return
+            let url = "https://sis.jhu.edu/api/classes?key=IwMTzqj8K5swInud8F5s7cAsxPRHOCtZ&Term=" + semester + "&CourseNumber=" + courseNumber;
+            let task = URLSession.shared.dataTask(with: URL(string:url)!) { (data, response, error) in
+                if let error = error {
+                    print("error: \(error)")
+                } else {
+                    if let response = response as? HTTPURLResponse {
+                        print("statusCode: \(response.statusCode)")
                     }
-//                    print(dateee)
-                    if let fooOffset = self.week.firstIndex(where: {$0 == dateee}) {
-                        print("found date in current week: \(dateee)")
-                        print(fooOffset)
+                    if let data = data {
                         
-                        let course1: Course = Course(name: data["courseName"] as! String, num: data["courseNum"] as! String , date: data["date"] as! String , weekday: fooOffset , location: data["location"] as! String, prof: data["prof"] as! String, startTime: data["startTime"] as! Double, duration: data["duration"] as! Double, syllabus: data["syllabus"] as! [String], locationURL: data["locationURL"] as! String, gradescopeURL: data["gradescopeURL"] as! String, webURL: data["webURL"] as! String)
-                        self.courses.append(course1)
-                    } else {
-                        print("nono")
+                        if let books = try? JSONDecoder().decode([Book].self, from: data) {
+                            //print(books)
+                            booooks.append(contentsOf: books)
+                        } else {
+                            print("Invalid Response")
+                        }
                     }
                 }
-                showCourses(courses: courses)
-                
-                
-//                self.view.addSubview(event(title:self.courses[0].name, day: self.courses[0].weekday, start: self.courses[0].startTime, duration: self.courses[0].duration))
+                completion(booooks, nil)
             }
-        }
-        
+            task.resume()
     }
     
     @objc func dateChange(datePicker: UIDatePicker) {
@@ -188,9 +236,10 @@ class ScheduleVC: UIViewController,NewEventDelegate {
                 self.present(controller,animated: true,completion: nil)
             }));
             let oneHour = self.view1.frame.height / 17
-            let y = (course.startTime - 7.0 + 0.6) * oneHour
+            let y = (course.startTime - 7.0 + 0.8) * oneHour
             courseButton.setTitle(course.name, for: .normal)
-            courseButton.backgroundColor = .blue
+            courseButton.titleLabel?.lineBreakMode = .byWordWrapping
+            courseButton.backgroundColor = UIColor(red: 102/255, green: 250/255, blue: 51/255, alpha: 0.5)
             courseButton.setTitleColor(.black, for: .normal);
             courseButton.frame = CGRect(x: 0, y: y, width: self.view1.frame.width, height: oneHour*course.duration);
             switch (course.weekday)  {
@@ -210,36 +259,88 @@ class ScheduleVC: UIViewController,NewEventDelegate {
             
         }
     }
-    
-    @IBAction func buttonTapped(_ sender: UIButton) {
-            print("Button tapped!")
+    func displayCourses(books: [Book]) {
+        for book in books {
+            for section in book.SectionDetails {
+                for meeting in section.Meetings {
+                    print(meeting.DOW)
+                    var days = parseDOW(DOW:meeting.DOW)
+                    let ss = meeting.Times
+                    let startStr = ss.substring(with: 0..<8);
+                    let endStr = ss.substring(with: 11..<19);
+                    let startTime = parseTime(time: startStr)
+                    let endTime = parseTime(time: endStr)
+                    for day in days {
+                    let courseButton = UIButton(type: .system, primaryAction: UIAction(title: "Button Title", handler: { _ in
+                        let controller =
+                        self.storyboard?.instantiateViewController(withIdentifier: "ViewEventStoryboard") as! ViewController
+                        // controller.course = course
+                        self.present(controller,animated: true,completion: nil)
+                    }));
+                    let oneHour = self.view1.frame.height / 17
+                    let y = (startTime - 7.0 + 0.7) * oneHour
+                    courseButton.setTitle(book.Title, for: .normal)
+                    courseButton.backgroundColor = UIColor(red: 102/255, green: 250/255, blue: 51/255, alpha: 0.5)
+                    courseButton.setTitleColor(.black, for: .normal);
+                    courseButton.frame = CGRect(x: 0, y: y, width: self.view1.frame.width, height: oneHour*(endTime - startTime));
+                    
+                        switch (day)  {
+                        case "M":
+                            self.view1.addSubview(courseButton)
+                        case "T":
+                            self.view2.addSubview(courseButton)
+                        case "W":
+                            self.view3.addSubview(courseButton)
+                        case "Th":
+                            self.view4.addSubview(courseButton)
+                        case "F":
+                            self.view5.addSubview(courseButton)
+                        default:
+                            self.view1.addSubview(courseButton)
+                        }
+                    }
+                }
+            }
         }
-
-    // day 0: Monday
-    func event (title: String, day: Double, start: Double, duration: Double) -> UIView {
-        let x = monday + day * width
-        let y = eight + (start - 8.0) * hour
-        let h = duration * hour
-        
-        let myNewView=UIView(frame: CGRect(x: x, y: y, width: width, height: h))
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 12)
-        label.textAlignment = .left
-        label.text = " " + title
-        label.textColor = .white
-        label.frame = CGRect(x: 0, y: 0, width: width, height: 20)
-        
-        myNewView.addSubview(label)
-        myNewView.backgroundColor=UIColor(red: 0, green: 0.6353, blue: 0.9765, alpha: 1.0)
-        myNewView.layer.cornerRadius = 5
-        return myNewView
     }
     
+    
+    func parseTime (time: String) -> Double{
+        let hour = Int(time.substring(with: 0..<2)) ?? 0
+        let minutes = Int(time.substring(with: 3..<5)) ?? 0
+        var res:Double = Double(hour) + Double(Double(minutes) / 60.0)
+        if (time.substring(with: 6..<8) == "PM" && time.substring(with: 0..<2) != "12") {
+            res += 12.0;
+        }
+        return res;
+    }
+    
+    func parseDOW (DOW: String) -> [String] {
+        var days: [String] = []
+        var i :Int = 0
+        while (i < DOW.count) {
+            let day = DOW.substring(with: i..<(i+1))
+            
+            if (i+1 < DOW.count && DOW.substring(with: i..<i+2)=="Th") {
+                days.append(DOW.substring(with: i..<i+2))
+                i += 1
+            } else {
+                days.append(day)
+            }
+            i += 1
+        }
+        return days
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Button tapped!")
+    }
+
+    // day 0: Monday
     @IBAction func addEvent(_ sender: Any) {
         let controller =
         storyboard?.instantiateViewController(withIdentifier: "AddEventStoryboard") as! AddEventViewController
         controller.modalPresentationStyle = .fullScreen
-        controller.delegate = self
+//        controller.delegate = self
         present(controller,animated: true,completion: nil)
     }
 
@@ -277,4 +378,3 @@ extension ScheduleVC: UITextFieldDelegate {
     }
     
 }
-
