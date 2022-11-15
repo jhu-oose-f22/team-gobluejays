a77d9589d9e177d69159c2f6ca52a83821794698
davidliuzw <davidliuzw18@163.com>
2022-11-15 11:46:56 -0500

    filter activity

---
+++ GoBlueJays/Controller/activityFilter.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityFilter.swift
+//  GoBlueJays
+//
+//  Created by david on 11/15/22.
+//
+
+import UIKit
+
+class activityFilter: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
